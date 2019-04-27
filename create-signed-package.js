5921065607e156d8cf71acc7282a23f6e16de70f
Bronley Plumb <bronley@gmail.com>
2019-04-27 07:39:22 -0400

    Added added script to generate signed package

---
+++ create-signed-package.js
@@ -0,0 +1,17 @@
+var rokuDeploy = require('roku-deploy');
+rokuDeploy.deployAndSignPackage({
+    files: [
+        "manifest",
+        "source/**/*.*",
+        "images/**/*.*"
+    ],
+    host: '192.168.1.103',
+    password: 'password',
+    signingPassword: 'SECRET'
+    //other options if necessary
+}).then(function (pathToSignedPackage) {
+    console.log('Signed package created at ', pathToSignedPackage);
+}, function (err) {
+    //it failed
+    console.error(err);
+});
