c215915dcb174d1a35537d68039bb4b750a767dd
Bronley Plumb <bronley@gmail.com>
2020-11-17 15:05:53 -0500

    package script read from bsconfig.json

--- create-signed-package.js
+++ create-signed-package.js
@@ -1,13 +1,6 @@
 var rokuDeploy = require('roku-deploy');
 rokuDeploy.deployAndSignPackage({
-    files: [
-        "manifest",
-        "source/**/*.*",
-        "images/**/*.*"
-    ],
     outFile: 'PlumMediaCenter',
-    host: '192.168.1.104',
-    password: 'password',
     signingPassword: '2XPxZpIzgplvjaT+FIk74g=='
     //other options if necessary
 }).then(function (pathToSignedPackage) {
