cc690001c2d11dc6ec83590b1cd5e3c5818194b7
Bronley <bronley@gmail.com>
2020-12-25 00:52:08 -0500

    Fixed bs deployment issue.

--- create-signed-package.js
+++ create-signed-package.js
@@ -1,7 +1,14 @@
 var rokuDeploy = require('roku-deploy');
 rokuDeploy.deployAndSignPackage({
+    rootDir: "dist",
+    retainStagingFolder: true,
+    stagingFolderPath: "out/.roku-deploy-staging",
     outFile: 'PlumMediaCenter',
-    signingPassword: '2XPxZpIzgplvjaT+FIk74g=='
+    signingPassword: '2XPxZpIzgplvjaT+FIk74g==',
+    files: [
+        "**/*",
+        "!**/*.map"
+    ]
     //other options if necessary
 }).then(function (pathToSignedPackage) {
     console.log('Signed package created at ', pathToSignedPackage);
